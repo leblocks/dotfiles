a3cfd7c17bd9dcec4dab594b651553a3ceefa9f6
alexander.g <alexander.g@qualisystems.com>
2023-10-19 21:34:38 +0300

    feat: powershell bootstrapping on various os

---
+++ bootstrap/alpine.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/ strict mode for sh
+set -euox
+
+# script is taken from here https://learn.microsoft.com/en-us/powershell/scripting/install/install-alpine?view=powershell-7.3
+POWERSHELL_VERSION=7.3.8
+
+# install the requirements
+apk add --no-cache \
+    ca-certificates \
+    less \
+    ncurses-terminfo-base \
+    krb5-libs \
+    libgcc \
+    libintl \
+    libssl1.1 \
+    libstdc++ \
+    tzdata \
+    userspace-rcu \
+    zlib \
+    icu-libs \
+    curl
+
+apk -X https://dl-cdn.alpinelinux.org/alpine/edge/main add --no-cache \
+    lttng-ust
+
+curl -L https://github.com/PowerShell/PowerShell/releases/download/v"${POWERSHELL_VERSION}"/powershell-"${POWERSHELL_VERSION}"-linux-alpine-x64.tar.gz -o /tmp/powershell.tar.gz
+
+mkdir -p /opt/microsoft/powershell/7
+tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+chmod +x /opt/microsoft/powershell/7/pwsh
+ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+
---
+++ bootstrap/archlinux.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.3.8/powershell-7.3.8-linux-x64.tar.gz
+mkdir -p /opt/microsoft/powershell/7
+tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+chmod +x /opt/microsoft/powershell/7/pwsh
+ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+
