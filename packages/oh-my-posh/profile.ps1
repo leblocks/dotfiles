291dc228df25fc565fc44aa36683f478c7f8dfc9
alexander.g <alexander.g@qualisystems.com>
2023-10-30 11:25:10 +0200

    feat: added top10 command to profile

--- packages/oh-my-posh/profile.ps1
+++ packages/oh-my-posh/profile.ps1
@@ -1,6 +1,13 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/material.omp.json" | Invoke-Expression
 
+function Get-TopTenProcesses {
+    Get-Process | Sort-Object WS, CPU -Descending | Select-Object -First 10
+}
+
+Set-Alias top10 Get-TopTenProcesses
+
 if ($IsWindows) {
     Set-Alias ll Get-ChildItem
     Set-Alias which Get-Command
 }
+
