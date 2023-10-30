7dc5c6a9f216751b66c52ad76da6ca085678be23
sashag <a.f.gurevich@gmail.com>
2023-10-30 15:08:39 +0200

    feat: git configuration

--- packages/git/configure.ps1
+++ packages/git/configure.ps1
@@ -1,3 +1,11 @@
 . $PSScriptRoot/../../utils.ps1
 
 Test-Dependencies(@("git"))
+
+$pathToLink = Join-Path $HOME ".gitconfig"
+$pathToGitConfig = Join-Path $PSScriptRoot ".gitconfig"
+
+# recreate gitconfig link
+Remove-Item -Path $pathToLink -Force -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path $pathToLink -Target $pathToGitConfig -Force
+
