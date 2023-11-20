bbfc96705cb9086146d35e3e41ca808a51c00158
sashag <a.f.gurevich@gmail.com>
2023-11-20 17:29:57 +0200

    feat: added vsvim configuration tests

---
+++ test/vsvim.tests.ps1
@@ -0,0 +1,14 @@
+. $PSScriptRoot/../utils.ps1
+
+Describe 'vsvim' -Skip:$IsLinux {
+    It 'link to .vsvrimrc exists' {
+        Test-Path "$HOME/.vsvimrc" | Should -Be $true
+    }
+
+    It 'link to .vsvimrc has correct content' {
+        $link = "$HOME/.vsvimrc"
+        $conf = "$PSScriptRoot/../packages/vsvim/.vsvimrc"
+        Get-Content $link | Should -Be (Get-Content $conf)
+    }
+}
+
