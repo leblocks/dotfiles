f7c78a7e3bccd4595ee2ec6623de7a68ba5f9c4e
sashag <a.f.gurevich@gmail.com>
2024-01-14 23:14:39 +0200

    chore: upgrade pwsh version

--- bootstrap/archlinux.sh
+++ bootstrap/archlinux.sh
@@ -7,8 +7,10 @@ SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 POWERSHELL_VERSION="$(cat "${SCRIPT_DIRECTORY}"/.version)"
 
 curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v"${POWERSHELL_VERSION}"/powershell-"${POWERSHELL_VERSION}"-linux-x64.tar.gz
+rm /opt/microsoft/powershell/7 -rf
 mkdir -p /opt/microsoft/powershell/7
 tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
 chmod +x /opt/microsoft/powershell/7/pwsh
+rm /usr/bin/pwsh
 ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 
