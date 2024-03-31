49fb211b7ba10d21a9d6b13fa3145d3746d752bb
sashag <a.f.gurevich@gmail.com>
2024-03-31 09:44:42 +0300

    feat: tests for glazewm installation

---
+++ test/packages/glazewm.tests.ps1
@@ -0,0 +1,14 @@
+. $PSScriptRoot/../../utils.ps1
+
+Describe 'glazewm' {
+    It 'link to .glazewm configuration exists' {
+        Test-Path "$HOME/.glaze-wm/config.yaml" | Should -Be $true
+    }
+
+    It 'link to .glazewm config has correct content' {
+        $link = "$HOME/.glaze-wm/config.yaml"
+        $conf = "$PSScriptRoot/../../packages/glazewm/.glaze-wm/config.yaml"
+        Get-Content $link | Should -Be (Get-Content $conf)
+    }
+}
+
