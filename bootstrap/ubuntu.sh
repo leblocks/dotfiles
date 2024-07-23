23e61bba4ab2a9cbe9caa038117bb5ea7f2079e7
sashag <a.f.gurevich@gmail.com>
2024-07-23 11:17:33 +0300

    feat: additional work on ubuntu bootstrap

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -5,8 +5,17 @@ set -euox
 
 
 apt-get update
+
 DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
-apt-get install -y wget git curl apt-transport-https software-properties-common 
+
+apt-get install -y \
+    wget \
+    git \
+    curl \
+    apt-transport-https \
+    software-properties-common \
+    build-essential
+
 source /etc/os-release
 wget -q "https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb"
 dpkg -i packages-microsoft-prod.deb
@@ -14,9 +23,13 @@ rm packages-microsoft-prod.deb
 apt-get update
 apt-get install -y powershell
 
+# install linuxbrew
 wget "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
-
 chmod u+x install.sh
-
 NONINTERACTIVE=1 ./install.sh
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /root/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+brew install gcc
 
