abf494e542c7f2f09135470f6d816cd04d40f7fa
sashag <a.f.gurevich@gmail.com>
2024-07-28 09:49:33 +0300

    feat: added test to check if packages were installed correctly

---
+++ test/packages/packages.tests.ps1
@@ -0,0 +1,34 @@
+
+function Get-PackagesList {
+    $packages = Join-Path $PSScriptRoot ".." ".." "packages" "packages.json"
+    Get-Content $packages | ConvertFrom-Json
+}
+
+BeforeAll {
+    . $PSScriptRoot/../../utils.ps1
+    $exceptions = @{
+        neovim = 'nvim'
+        ripgrep = 'rg'
+        bottom = 'btm'
+    }
+}
+
+Describe 'packages from packages.json were installed' {
+    It "common package <_> is installed" -ForEach (Get-PackagesList).common {
+        if ($exceptions.ContainsKey($_)) {
+            Test-Command $exceptions[$_] | Should -Be $true
+        } else {
+            Test-Command $_ | Should -Be $true
+        }
+    }
+
+    It "linux package <_> is installed" -Skip:$IsWindows -ForEach (Get-PackagesList).linux {
+        Test-Command $_ | Should -Be $true
+    }
+
+    It "windows package <_> is installed" -Skip:$IsLinux -ForEach (Get-PackagesList).linux {
+        Test-Command $_ | Should -Be $true
+    }
+}
+
+
