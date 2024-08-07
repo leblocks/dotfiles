c3d7522893273b34051903e5b2ea99d763b6bc69
sashag <a.f.gurevich@gmail.com>
2024-08-07 23:18:27 +0300

    feat: custom windows installation script

--- custom/windows.ps1
+++ custom/windows.ps1
@@ -1,10 +1,25 @@
+<#
+    custom windows installation where
+    git, npm and other packages are already installed and managed
+    by other admin
+#>
 
 . (Join-Path $PSScriptRoot ".." "utils.ps1")
 
-Test-Dependencies(@("scoop"))
+if (-Not (Test-Command -Command "scoop")) {
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+}
+
+try {
+    Get-PythonExecutable
+}
+catch {
+    "scoop install python" | Invoke-FailFastExpression
+}
 
 . (Join-Path $PSScriptRoot ".." "dotfiles.ps1") install
+. (Join-Path $PSScriptRoot ".." "dotfiles.ps1") configure "oh-my-posh"
+. (Join-Path $PSScriptRoot ".." "dotfiles.ps1") configure glazewm
 . (Join-Path $PSScriptRoot ".." "dotfiles.ps1") configure neovim
 . (Join-Path $PSScriptRoot ".." "dotfiles.ps1") configure vsvim
-. (Join-Path $PSScriptRoot ".." "dotfiles.ps1") configure "oh-my-posh"
 
