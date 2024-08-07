75bab6220e4e4946697c63cc9fe21b6f546599ff
sashag <a.f.gurevich@gmail.com>
2024-08-07 23:44:02 +0300

    chore: no need to automatically add glazewm to autostart

--- packages/glazewm/configure.ps1
+++ packages/glazewm/configure.ps1
@@ -4,13 +4,6 @@ Test-Dependencies(@("winget"))
 
 winget install GlazeWM
 
-# add to autostart like admin
-$autoStart = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\run"
-$pathToExe = (which glazewm).Path
-
-Remove-ItemProperty -Path $autoStart -Name glazewm -ErrorAction SilentlyContinue
-New-ItemProperty -Path $autoStart -Name glazewm -Value $pathToExe
-
 # recreate config link
 LinkToHome $PSScriptRoot ".glaze-wm"
 
