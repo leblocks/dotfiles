fbfe605aacd63fb5d943dc627e4533e9d96ca7bf
sashag <a.f.gurevich@gmail.com>
2024-09-16 09:44:49 +0300

    fix: add --global for git config

--- packages/git/configure.ps1
+++ packages/git/configure.ps1
@@ -4,5 +4,5 @@ Test-Dependencies(@("git"))
 
 # linking .gitconfig to the $HOME is not that great
 # it will override custom defined stuff, so I'll do inlcude instead
-"git config include.path `"$(Resolve-Path (Join-Path $PSScriptRoot .gitconfig))`"" | Invoke-FailFastExpression
+"git config --global include.path `"$(Resolve-Path (Join-Path $PSScriptRoot .gitconfig))`"" | Invoke-FailFastExpression
 
