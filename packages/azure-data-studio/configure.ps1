482df0ac766d99a9f9dd8d24fffeb453b6364b05
sashag <a.f.gurevich@gmail.com>
2024-10-01 09:48:06 +0300

    feat: added installation and configuration of azure-data-studio

---
+++ packages/azure-data-studio/configure.ps1
@@ -0,0 +1,26 @@
+. $PSScriptRoot/../../utils.ps1
+
+Test-Dependencies(@("winget"))
+
+"winget install Microsoft.AzureDataStudio" | Invoke-FailFastExpression
+
+$pathToExtension = Join-Path ([System.IO.Path]::GetTempPath()) "extension.vsix"
+Remove-Item -Path $pathToExtension -Force -ErrorAction SilentlyContinue
+
+# TODO download latest release
+Invoke-WebRequest `
+    -Uri "https://github.com/VSCodeVim/Vim/releases/download/v1.28.1/vim-1.28.1.vsix" `
+    -OutFile $pathToExtension `
+    -MaximumRetryCount 5 `
+    -RetryIntervalSec 3
+
+Write-Output @(
+    "azuredatastudio.cmd --install-extension $pathToExtension"
+) -NoEnumerate | Invoke-Expressions
+
+
+$pathToLink = Join-Path $env:AppData "azuredatastudio" "User" "settings.json"
+
+Remove-Item -Path $pathToLink -Force -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path $pathToLink -Target (Join-Path $PSScriptRoot  "settings.json") -Force
+
