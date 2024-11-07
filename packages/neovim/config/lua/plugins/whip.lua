1ca5273407c9c329d8beb851010f7789d832b30c
Sasha Gurevich <sagurevich@microsoft.com>
2024-11-07 11:22:51 +0200

    fix: init whip only if there is a folder for it

--- packages/neovim/config/lua/plugins/whip.lua
+++ packages/neovim/config/lua/plugins/whip.lua
@@ -1,5 +1,11 @@
 local os = require('os')
 
-require('whip').setup({
-    dir = (os.getenv('OneDrive') or os.getenv('USERPROFILE') or os.getenv('HOME')) .. '/whip'
-})
+local whip_path = (os.getenv('OneDrive') or os.getenv('USERPROFILE') or os.getenv('HOME')) .. '/whip'
+print(whip_path)
+
+if vim.fn.isdirectory(whip_path) ~= 0 then
+    require('whip').setup({
+        dir = whip_path
+    })
+end
+
