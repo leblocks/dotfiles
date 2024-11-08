9ca1c9683fe2c6a75fbce16ddaa2adb057dc122d
Sasha Gurevich <sagurevich@microsoft.com>
2024-11-08 20:56:19 +0200

    fix: removing leftover print in config

--- packages/neovim/config/lua/plugins/whip.lua
+++ packages/neovim/config/lua/plugins/whip.lua
@@ -1,7 +1,6 @@
 local os = require('os')
 
 local whip_path = (os.getenv('OneDrive') or os.getenv('USERPROFILE') or os.getenv('HOME')) .. '/whip'
-print(whip_path)
 
 if vim.fn.isdirectory(whip_path) ~= 0 then
     require('whip').setup({
