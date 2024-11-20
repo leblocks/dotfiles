e8b00e0b721ddaf9fd4a93bd25d53c111778a61c
Sasha Gurevich <sagurevich@microsoft.com>
2024-11-20 13:11:14 +0200

    chore: cleanup of plugins.lua

--- packages/neovim/config/lua/plugins.lua
+++ packages/neovim/config/lua/plugins.lua
@@ -13,38 +13,46 @@ return packer.startup(function(use)
     use('nvim-telescope/telescope-fzy-native.nvim')
     use('nvim-telescope/telescope-ui-select.nvim')
 
+    -- treesitter
     use('nvim-treesitter/nvim-treesitter')
+    use('windwp/nvim-ts-autotag')
 
     use('nvim-tree/nvim-tree.lua')
 
+    -- hail to the tpope
     use('tpope/vim-surround')
     use('tpope/vim-fugitive')
     use('tpope/vim-commentary')
 
+    -- lsp
     use('neovim/nvim-lspconfig')
     use('ray-x/lsp_signature.nvim')
     use('ray-x/cmp-treesitter')
+    use('seblj/roslyn.nvim')
 
+    -- completion
     use('hrsh7th/nvim-cmp')
     use('hrsh7th/cmp-buffer')
     use('hrsh7th/cmp-nvim-lsp')
 
+    -- colors
     use('morhetz/gruvbox')
     use('Mofiqul/vscode.nvim')
     use('ntk148v/komau.vim')
+    use('tyrannicaltoucan/vim-deep-space')
 
     use('kyazdani42/nvim-web-devicons')
     use('nathom/filetype.nvim')
 
+    -- ui
     use({ 'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true } })
     use({ 'alvarosevilla95/luatab.nvim', requires = 'nvim-tree/nvim-web-devicons' })
+
     -- note taking
-    use({ 'slugbyte/whip.nvim'})
+    use('slugbyte/whip.nvim')
 
     -- center buffer
     use({"shortcuts/no-neck-pain.nvim", tag = "*" })
-
-    use('windwp/nvim-ts-autotag')
 
     -- nvim-dap
     use('mfussenegger/nvim-dap')
@@ -53,15 +61,13 @@ return packer.startup(function(use)
 
     -- nvim-lint for cases when there is no lsp server
     use('mfussenegger/nvim-lint')
+
     -- keep track of configuration performance
     use('dstein64/vim-startuptime')
 
     -- copilot integration
     use('github/copilot.vim')
-    use({'CopilotC-Nvim/CopilotChat.nvim', requires = { 'nvim-lua/plenary.nvim', 'github/copilot.vim' } })
-
-    -- roslyn integration
-    use('seblj/roslyn.nvim')
+    use({ 'CopilotC-Nvim/CopilotChat.nvim', requires = { 'nvim-lua/plenary.nvim', 'github/copilot.vim' } })
 
     if packer_bootstrap then
         packer.sync()
