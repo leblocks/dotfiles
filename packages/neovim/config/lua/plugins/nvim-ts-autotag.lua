39dac7839893581528af78ad4291eba5b99d94e9
Sasha Gurevich <sagurevich@microsoft.com>
2024-11-20 13:11:39 +0200

    chore: added aliases for autotag

--- packages/neovim/config/lua/plugins/nvim-ts-autotag.lua
+++ packages/neovim/config/lua/plugins/nvim-ts-autotag.lua
@@ -1,9 +1,14 @@
 require('nvim-ts-autotag').setup({
-  opts = {
-    -- Defaults
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = false -- Auto close on trailing </
-  },
-})
+    opts = {
+        enable_close = true,      -- Auto close tags
+        enable_rename = true,     -- Auto rename pairs of tags
+        enable_close_on_slash = false -- Auto close on trailing </
+    },
 
+    aliases = {
+        ["csproj"] = "html",
+        ["props"] = "html",
+        ["targets"] = "html",
+        ["resx"] = "html"
+    }
+})
