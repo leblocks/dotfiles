49000ef72e18769d1d060b414c7e2c39a440591d
Sasha Gurevich <sagurevich@microsoft.com>
2024-11-24 10:42:26 +0200

    fix: removed unnecessary binding

--- packages/neovim/config/lua/keymaps.lua
+++ packages/neovim/config/lua/keymaps.lua
@@ -127,5 +127,4 @@ map('n', '<Leader>00', ':terminal pwsh -NoLogo<CR>', default_opts)
 -- buffer
 map('n', '<Leader>br', ':lua RenameBuffer()<CR>', default_opts)
 map('n', '<Leader>bd', ':bdelete<CR>', default_opts)
-map('n', '<Leader>bD', ':bdelete!<CR>', default_opts)
 
