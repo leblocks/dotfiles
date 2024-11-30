2f9b02c804dd6e550df85431fd3a7f6f0a68c7c2
Sasha Gurevich <sagurevich@microsoft.com>
2024-11-30 22:32:34 +0200

    chore: disable generation on empty buffer gutentag

--- packages/neovim/config/lua/plugins/vim-gutentags.lua
+++ packages/neovim/config/lua/plugins/vim-gutentags.lua
@@ -9,7 +9,7 @@ vim.cmd [[
     let g:gutentags_generate_on_new = 1
     let g:gutentags_generate_on_missing = 1
     let g:gutentags_generate_on_write = 1
-    let g:gutentags_generate_on_empty_buffer = 1
+    let g:gutentags_generate_on_empty_buffer = 0
 
     let g:gutentags_ctags_extra_args = [
           \ '--tag-relative=yes',
