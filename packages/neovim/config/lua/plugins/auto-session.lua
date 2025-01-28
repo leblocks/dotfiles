dc8e359644559e5ec3346ea1326115c3fda0c7bb
Sasha Gurevich <sagurevich@microsoft.com>
2025-01-28 19:44:57 +0200

    fix: do not save terminal buffers

--- packages/neovim/config/lua/plugins/auto-session.lua
+++ packages/neovim/config/lua/plugins/auto-session.lua
@@ -1,4 +1,4 @@
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,localoptions"
 
 require('auto-session').setup({
     enabled = true, -- Enables/disables auto creating, saving and restoring
