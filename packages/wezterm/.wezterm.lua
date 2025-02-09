228eaa375d05afa9ec253c4bc4329f716907bc3f
Sasha Gurevich <sagurevich@microsoft.com>
2025-02-09 14:20:51 +0200

    fix: defined override function in a more correct way

--- packages/wezterm/.wezterm.lua
+++ packages/wezterm/.wezterm.lua
@@ -2,19 +2,8 @@
 local wezterm = require('wezterm')
 local os = require('os')
 
-local is_devbox = string.lower(os.getenv('IsDevBox') or '') == 'true'
-local devbox_config_path = os.getenv('DEVBOX_WEZTERM_CONFIG') or ''
-
 -- This will hold the configuration.
 local config = wezterm.config_builder();
-
-if is_devbox ~= false then
-    config.prefer_egl = true
-    local status, err = pcall(function() dofile(devbox_config_path) end)
-    if not status then
-        print('failed to load external configuration from "' .. devbox_config_path .. '" error: ' .. err)
-    end
-end
 
 -- general
 config.default_prog = { 'pwsh', '-NoLogo', '-NoProfileLoadTime' }
@@ -79,6 +68,19 @@ config.keys = {
 for i = 1, 9 do
   -- LEADER + number to activate that tab
   table.insert(config.keys, { key = tostring(i), mods = 'LEADER', action = act.ActivateTab(i - 1), })
+end
+
+local is_devbox = string.lower(os.getenv('IsDevBox') or '') == 'true'
+local devbox_config_path = os.getenv('DEVBOX_WEZTERM_CONFIG') or ''
+
+-- pass config inside and override it
+if is_devbox ~= false then
+    local status, err = pcall(function() dofile(devbox_config_path) end)
+    if not status then
+        print('failed to load external configuration from "' .. devbox_config_path .. '" error: ' .. err)
+    else
+        set_configuraiton_overrides(config)
+    end
 end
 
 return config
