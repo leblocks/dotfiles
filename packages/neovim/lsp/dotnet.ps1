4f1b8e14bf4e63a1220fe3195825cbb8b6ebbaeb
Sasha Gurevich <sagurevich@microsoft.com>
2025-02-17 21:14:17 +0200

    feat: update roslyn version

--- packages/neovim/lsp/dotnet.ps1
+++ packages/neovim/lsp/dotnet.ps1
@@ -1,6 +1,6 @@
 param([Parameter(Position=0, Mandatory=$True)] [string] $rootPath)
 
-$ROSLYN_VERSION = "4.13.0-2.24563.22"
+$ROSLYN_VERSION = "5.0.0-1.25111.6"
 
 . $PSScriptRoot/../../../utils.ps1
 
