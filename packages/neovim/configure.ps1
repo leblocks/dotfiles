94d86ba3e5148e0de0a92876eb1a81891eadf772
Sasha Gurevich <sagurevich@microsoft.com>
2025-03-04 21:55:44 +0200

    feat: made ctags manual, to stop from spawning ctags

--- packages/neovim/configure.ps1
+++ packages/neovim/configure.ps1
@@ -8,26 +8,39 @@ $pathToLink = $IsWindows ? (Join-Path $HOME "AppData" "Local" "nvim")
 if ($IsLinux) {
     $linuxPathToConfig = Join-Path $HOME ".config"
     if (-Not (Test-Path $linuxPathToConfig)) {
-        New-Item -Path $linuxPathToConfig -ItemType Directory -Force
+        New-Item `
+            -ItemType Directory `
+            -Path $linuxPathToConfig `
+            -Force
     }
 }
 
 $pathToConfig = Join-Path $PSScriptRoot "config"
 
 # recreate link to neovim config to $HOME folder
-Remove-Item -Path $pathToLink -Force -ErrorAction SilentlyContinue
-New-Item -ItemType SymbolicLink -Path $pathToLink -Target $pathToConfig -Force
+Remove-Item `
+    -Path $pathToLink `
+    -ErrorAction SilentlyContinue `
+    -Force
+
+New-Item `
+    -ItemType SymbolicLink `
+    -Path $pathToLink `
+    -Target $pathToConfig `
+    -Force
 
 $pathToPacker = $IsWindows ? (Join-Path $HOME "\AppData\Local\nvim-data\site\pack\packer\start\packer.nvim")
     : (Join-Path $HOME "/.local/share/nvim/site/pack/packer/start/packer.nvim")
 
 # ensure packer is bootstrapped
 if (-Not (Test-Path $pathToPacker)) {
-    "git clone --depth 1 https://github.com/wbthomason/packer.nvim $pathToPacker" | Invoke-FailFastExpression
+    "git clone --depth 1 https://github.com/wbthomason/packer.nvim $pathToPacker"
+        | Invoke-FailFastExpression
 }
 
 # update plugins
-"nvim --headless -c `"autocmd User PackerComplete quitall`" -c `"PackerSync`"" | Invoke-FailFastExpression
+"nvim --headless -c `"autocmd User PackerComplete quitall`" -c `"PackerSync`""
+    | Invoke-FailFastExpression
 
 $pathToLanguageTools = Join-Path $HOME ".neovim"
 New-Folder $pathToLanguageTools
@@ -39,4 +52,6 @@ New-Folder $pathToLanguageTools
 . $PSScriptRoot/lsp/powershell.ps1 $pathToLanguageTools
 . $PSScriptRoot/lsp/dotnet.ps1 $pathToLanguageTools
 . "$PSScriptRoot/lsp/vscode-langservers-extracted.ps1" $pathToLanguageTools
+
+Set-EnvironmentVariable "NEOVIM_CTAGS_EXECUTABLE" (Get-Command ctags).Source
 
