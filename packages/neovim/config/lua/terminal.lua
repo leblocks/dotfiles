8e5a9b7eb787f8ee48295eab82d4c6768b0dcb25
Sasha Gurevich <sagurevich@microsoft.com>
2025-03-12 20:56:21 +0200

    feat: list terminals function

--- packages/neovim/config/lua/terminal.lua
+++ packages/neovim/config/lua/terminal.lua
@@ -25,6 +25,35 @@ local function close_terminal_buffers()
     end
 end
 
+local function format_terminal_name(terminal_buffer)
+    local title = vim.fn.getbufvar(terminal_buffer, 'term_title')
+    local name = vim.api.nvim_buf_get_name(tonumber(terminal_buffer))
+    return '(' .. title .. ') ' .. name
+end
+
+local function select_terminal()
+    local terminal_buffers = {}
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if (buf ~= vim.api.nvim_get_current_buf() and vim.fn.getbufvar(buf, '&buftype') == 'terminal') then
+            print(buf)
+            table.insert(terminal_buffers, tostring(buf))
+        end
+    end
+
+    vim.ui.select(terminal_buffers,
+        {
+            prompt = 'Select terminal:',
+            format_item = format_terminal_name,
+        },
+        function(buf)
+            if (buf == nil)
+            then
+                return
+            end
+            vim.api.nvim_set_current_buf(tonumber(buf))
+        end)
+end
+
 vim.api.nvim_create_autocmd({ 'TermOpen' },
     {
         pattern = '*',
@@ -52,3 +81,9 @@ vim.api.nvim_create_user_command('CloseTerminals', function()
     close_terminal_buffers()
 end, {})
 
+vim.api.nvim_create_user_command('SelectTerminal', function()
+    select_terminal()
+end, {})
+
+
+-- TODO terminals telescope picker
