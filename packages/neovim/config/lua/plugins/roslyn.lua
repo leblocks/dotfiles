762de8ae24bb9e456481b1e694212c38a26a4eb9
Sasha Gurevich <sagurevich@microsoft.com>
2025-03-23 12:07:03 +0200

    chore: breaking change alignment

--- packages/neovim/config/lua/plugins/roslyn.lua
+++ packages/neovim/config/lua/plugins/roslyn.lua
@@ -47,17 +47,6 @@ if enable_roslyn ~= false then
             "--logLevel=Information", "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()), "--stdio"
         },
 
-        -- NOTE: Set `filewatching` to false if you experience performance problems.
-        -- Defaults to true, since turning it off is a hack.
-        -- If you notice that the server is _super_ slow, it is probably because of file watching
-        -- Neovim becomes super unresponsive on some large codebases, because it schedules the file watching on the event loop.
-        -- This issue goes away by disabling this capability, but roslyn will fallback to its own file watching,
-        -- which can make the server super slow to initialize.
-        -- Setting this option to false will indicate to the server that neovim will do the file watching.
-        -- However, in `hacks.lua` I will also just don't start off any watchers, which seems to make the server
-        -- a lot faster to initialize.
-        filewatching = true,
-
         -- Optional function that takes an array of solutions as the only argument. Return the solution you
         -- want to use. If it returns `nil`, then it falls back to guessing the solution like normal
         -- Example:
