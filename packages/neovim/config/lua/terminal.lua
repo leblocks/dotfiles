df9cddf9c41e547efafec141da7a2350d067881e
Sasha Gurevich <sagurevich@microsoft.com>
2025-03-25 15:49:27 +0200

    feat: support terminal selection on creation

--- packages/neovim/config/lua/terminal.lua
+++ packages/neovim/config/lua/terminal.lua
@@ -3,15 +3,50 @@ local function terminal_config()
     vim.opt_local.relativenumber = false
 end
 
-local function get_launch_terminal_command()
-    current_directory = vim.uv.cwd()
-
-    -- prefer cmd on windows
-    if vim.loop.os_uname().sysname == "Windows_NT" then
-        return "cmd.exe /K clink inject && cd " .. current_directory
+local function get_current_buffer_directory()
+    local directory = vim.fn.expand('%:p:h')
+    -- terminal buffers have strange directory names
+    if (directory:find("term://")) then
+        return vim.uv.cwd()
     end
+    return directory
+end
 
-    return "pwsh -NoLogo -NoProfileLoadTime -WorkingDirectory " .. current_directory
+local function launch_terminal(callback)
+    local current_working_directory = vim.uv.cwd()
+    local current_buffer_directory = get_current_buffer_directory()
+
+    local commands = {
+        {
+            command = "cmd.exe /K clink inject && cd " .. current_working_directory,
+            description = "clink in cwd"
+        },
+        {
+            command = "cmd.exe /K clink inject && cd " .. current_buffer_directory,
+            description = "clink in buffer folder"
+        },
+        {
+            command = "pwsh -NoLogo -NoProfileLoadTime -WorkingDirectory " .. current_working_directory,
+            description = "pwsh in cwd"
+        },
+        {
+            command = "pwsh -NoLogo -NoProfileLoadTime -WorkingDirectory " .. current_buffer_directory,
+            description = "pwsh in buffer folder"
+        },
+    }
+
+    vim.ui.select(commands,
+        {
+            prompt = 'Select terminal to launch:',
+            format_item = function(item) return item.description end,
+        },
+        function(item)
+            if (item == nil)
+            then
+                return
+            end
+            callback(item.command)
+        end)
 end
 
 local function close_terminal_buffers()
@@ -36,7 +71,6 @@ local function select_terminal()
     local terminal_buffers = {}
     for _, buf in ipairs(vim.api.nvim_list_bufs()) do
         if (buf ~= vim.api.nvim_get_current_buf() and vim.fn.getbufvar(buf, '&buftype') == 'terminal') then
-            print(buf)
             table.insert(terminal_buffers, tostring(buf))
         end
     end
@@ -55,27 +89,34 @@ local function select_terminal()
         end)
 end
 
-vim.api.nvim_create_autocmd({ 'TermOpen' },
-    {
-        pattern = '*',
-        callback = terminal_config,
-        group = vim.api.nvim_create_augroup('terminal_config', { clear = true })
-    })
+vim.api.nvim_create_autocmd({ 'TermOpen' }, {
+    pattern = '*',
+    callback = terminal_config,
+    group = vim.api.nvim_create_augroup('terminal_config', { clear = true })
+})
 
 vim.api.nvim_create_user_command('Terminal', function()
-    vim.cmd(":e term://" .. get_launch_terminal_command())
+    launch_terminal(function(command)
+        vim.cmd(":e term://" .. command)
+    end)
 end, {})
 
 vim.api.nvim_create_user_command('TTerminal', function()
-    vim.cmd(":tabnew term://" .. get_launch_terminal_command())
+    launch_terminal(function(command)
+        vim.cmd(":tabnew term://" .. command)
+    end)
 end, {})
 
 vim.api.nvim_create_user_command('VTerminal', function()
-    vim.cmd(":vnew term://" .. get_launch_terminal_command())
+    launch_terminal(function(command)
+        vim.cmd(":vnew term://" .. command)
+    end)
 end, {})
 
 vim.api.nvim_create_user_command('STerminal', function()
-    vim.cmd(":split term://" .. get_launch_terminal_command())
+    launch_terminal(function(command)
+        vim.cmd(":split term://" .. command)
+    end)
 end, {})
 
 vim.api.nvim_create_user_command('CloseTerminals', function()
@@ -85,6 +126,3 @@ end, {})
 vim.api.nvim_create_user_command('SelectTerminal', function()
     select_terminal()
 end, {})
-
-
--- TODO terminals telescope picker
