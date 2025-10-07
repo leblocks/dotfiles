c4598e562ef97a0562f919dd466e60651f37e0de
Sasha Gurevich <sagurevich@microsoft.com>
2025-10-07 15:29:03 +0300

    feat(pwshlint): added test with format check

--- test/dotfiles.tests.ps1
+++ test/dotfiles.tests.ps1
@@ -1,8 +1,15 @@
-. $(Join-Path $PSScriptRoot .. utils.ps1)
+BeforeAll {
+    . $(Join-Path $PSScriptRoot .. utils.ps1)
+}
 
 Describe 'dotfiles' {
     It 'dotfiles command is on $PATH' {
-        Test-Command dotfiles.ps1| Should -Be $true
+        Test-Command dotfiles.ps1 | Should -Be $true
+    }
+
+    It 'dotfiles repository is formatted' -Tag Format {
+        Invoke-PSScriptAnalyzer (Join-Path -Path $PSScriptRoot -ChildPath "..")
+            | Should -BeNullOrEmpty
     }
 }
 
--- utils.ps1
+++ utils.ps1
@@ -1,19 +1,23 @@
 
-function Test-Command ([string] $Command) {
+function Test-Command ([string] $Command)
+{
     [bool](Get-Command $Command -ErrorAction SilentlyContinue)
 }
 
-function Test-Dependencies ([string[]] $packages) {
+function Test-Dependencies ([string[]] $packages)
+{
     $errors = $packages
-        | Where-Object { (-Not (Test-Command -Command $_)) }
-        | ForEach-Object { "Package '$_' could not be found." }
+    | Where-Object { (-Not (Test-Command -Command $_)) }
+    | ForEach-Object { "Package '$_' could not be found." }
 
-    if (($errors | Measure-Object).Count -gt 0) {
+    if (($errors | Measure-Object).Count -gt 0)
+    {
         throw [string]::Concat([Environment]::NewLine, $errors)
     }
 }
 
-function LinkToHome ([string] $folder, [string] $fileName) {
+function LinkToHome ([string] $folder, [string] $fileName)
+{
     $pathToLink = Join-Path $HOME $fileName
 
     Remove-Item `
@@ -28,40 +32,59 @@ function LinkToHome ([string] $folder, [string] $fileName) {
         -Force
 }
 
-function Get-PackageManager {
-    if (Test-Command -Command "brew") { return "brew" }
-    if (Test-Command -Command "scoop") { return "scoop" }
-    if (Test-Command -Command "pacman") { return "pacman" }
+function Get-PackageManager
+{
+    if (Test-Command -Command "brew")
+    { return "brew"
+    }
+    if (Test-Command -Command "scoop")
+    { return "scoop"
+    }
+    if (Test-Command -Command "pacman")
+    { return "pacman"
+    }
     throw "Could not find supported package manager for installation."
 }
 
-function Get-PackageManagerInstallCommand([string] $PackageManager, [string] $Package) {
-    switch($PackageManager) {
-        "brew" { return "brew install $Package" }
-        "scoop" { return "scoop install $Package" }
-        "pacman" {
+function Get-PackageManagerInstallCommand([string] $PackageManager, [string] $Package)
+{
+    switch($PackageManager)
+    {
+        "brew"
+        { return "brew install $Package"
+        }
+        "scoop"
+        { return "scoop install $Package"
+        }
+        "pacman"
+        {
             $sudo = ""
 
-            if (Test-Command -Command "sudo") {
+            if (Test-Command -Command "sudo")
+            {
                 $sudo = "sudo"
             }
 
             return "$sudo pacman -Sy --noconfirm $Package"
         }
-        default { throw "$PackageManager is not supported." }
+        default
+        { throw "$PackageManager is not supported."
+        }
     }
 }
 
-function Install-Packages([string] $PackageManager, [string[]] $Packages) {
+function Install-Packages([string] $PackageManager, [string[]] $Packages)
+{
     $counter = 0
-    foreach ($package in $Packages) {
+    foreach ($package in $Packages)
+    {
         $counter++
         $progress = [int](($counter / $Packages.Count) * 100)
 
         Get-PackageManagerInstallCommand `
             -PackageManager $PackageManager `
             -Package $package
-            | Invoke-FailFastExpression
+        | Invoke-FailFastExpression
 
         Write-Progress `
             -Activity "installing packages" `
@@ -70,10 +93,12 @@ function Install-Packages([string] $PackageManager, [string[]] $Packages) {
     }
 }
 
-function Set-EnvironmentVariable ([string] $name, [string] $value) {
+function Set-EnvironmentVariable ([string] $name, [string] $value)
+{
     $path = Join-Path $HOME ".environment.json"
 
-    if (-Not (Test-Path $path)) {
+    if (-Not (Test-Path $path))
+    {
         New-Item `
             -Path $path `
             -ItemType File `
@@ -86,40 +111,46 @@ function Set-EnvironmentVariable ([string] $name, [string] $value) {
     $environment = Get-Content $path | ConvertFrom-Json
     $exists = [bool](Get-Member -InputObject $environment -MemberType NoteProperty -Name $name)
 
-    if ($exists) {
+    if ($exists)
+    {
         $environment."$name" = $value
-    } else {
+    } else
+    {
         Add-Member `
-           -InputObject $environment `
-           -MemberType NoteProperty `
-           -Name $name `
-           -Value $value
+            -InputObject $environment `
+            -MemberType NoteProperty `
+            -Name $name `
+            -Value $value
     }
 
     $environment
-        | ConvertTo-Json
-        | Set-Content $path
+    | ConvertTo-Json
+    | Set-Content $path
 }
 
-function Add-PathEntry ([string] $pathEntry) {
+function Add-PathEntry ([string] $pathEntry)
+{
     $path = Join-Path $HOME ".path.json"
     # .path.json is an array of additional path entries, that
     # must be unique, we don't want duplicates
-    if (-Not (Test-Path $path)) {
+    if (-Not (Test-Path $path))
+    {
         $pathEntries = New-Object System.Collections.Generic.HashSet[string]
-    } else {
+    } else
+    {
         $pathEntries = [System.Collections.Generic.HashSet[string]](Get-Content $path | ConvertFrom-Json)
     }
 
     $pathEntries.Add($pathEntry)
-        | Out-Null
+    | Out-Null
 
     $pathEntries
-        | ConvertTo-Json
-        | Set-Content $path
+    | ConvertTo-Json
+    | Set-Content $path
 }
 
-function New-Folder ([string] $path) {
+function New-Folder ([string] $path)
+{
     Remove-Item `
         -Path $path `
         -ErrorAction SilentlyContinue `
@@ -132,26 +163,30 @@ function New-Folder ([string] $path) {
         -Force
 }
 
-function Write-Message ([string] $message) {
+function Write-Message ([string] $message)
+{
     Write-Host $message -ForegroundColor Green
 }
 
-function Get-Files {
+function Get-Files
+{
     Get-ChildItem . -Force
 }
 
-function Get-Ports {
+function Get-Ports
+{
     Get-NetTCPConnection | ForEach-Object {
-            Add-Member `
-                -InputObject $_ `
-                -MemberType NoteProperty `
-                -Value (Get-Process -Id $_.OwningProcess).Path `
-                -Name "Cmd" `
-                -PassThru
-        } | Select-Object "LocalAddress", "LocalPort", "RemoteAddress", "Remote-Port", "State", "OwningProcess", "Cmd"
+        Add-Member `
+            -InputObject $_ `
+            -MemberType NoteProperty `
+            -Value (Get-Process -Id $_.OwningProcess).Path `
+            -Name "Cmd" `
+            -PassThru
+    } | Select-Object "LocalAddress", "LocalPort", "RemoteAddress", "Remote-Port", "State", "OwningProcess", "Cmd"
 }
 
-function Invoke-FailFastExpression {
+function Invoke-FailFastExpression
+{
     param (
         [parameter(ValueFromPipeline)]
         [string]$command
@@ -159,18 +194,23 @@ function Invoke-FailFastExpression {
 
     $debug = $env:DOTFILES_DEBUG
 
-    try {
-        if ($null -eq $debug) {
+    try
+    {
+        if ($null -eq $debug)
+        {
             Invoke-Expression $command | Out-Null
-        } else {
+        } else
+        {
             Invoke-Expression $command
         }
-    } catch {
+    } catch
+    {
         throw "Invoke-FailFastExpression failed on $command with: $_"
     }
 }
 
-function Invoke-Expressions {
+function Invoke-Expressions
+{
     param (
         [parameter(ValueFromPipeline)]
         [string[]]$commands
@@ -178,7 +218,8 @@ function Invoke-Expressions {
 
     $counter = 0;
 
-    foreach ($command in $commands) {
+    foreach ($command in $commands)
+    {
         $counter++
         $progress = [int](($counter / $commands.Length) * 100)
 
@@ -194,49 +235,59 @@ function Invoke-Expressions {
 # python can be symlinked to python3
 # we have to check always that we are working
 # with python 3
-function Get-PythonExecutable {
-    if (Test-Command -Command "python") {
+function Get-PythonExecutable
+{
+    if (Test-Command -Command "python")
+    {
         $isPython3 = "python -V"
-            | Invoke-Expression
-            | Select-String -Pattern " 3." -Quiet
+        | Invoke-Expression
+        | Select-String -Pattern " 3." -Quiet
 
-        if ($isPython3) {
+        if ($isPython3)
+        {
             return (Get-Command "python").Source
         }
     }
 
-    if (Test-Command -Command "python3") {
+    if (Test-Command -Command "python3")
+    {
         return (Get-Command "python3").Source
     }
 
     throw "Could not find python 3 executable"
 }
 
-function Get-CurrentGitBranch {
-    if (-Not [bool](Get-Command git)) {
+function Get-CurrentGitBranch
+{
+    if (-Not [bool](Get-Command git))
+    {
         return ""
     }
 
     $gitBranchName = (git branch --show-current 2>&1).ToString()
 
-    if ($gitBranchName.Contains('fata')) {
+    if ($gitBranchName.Contains('fata'))
+    {
         return ""
     }
 
     return $gitBranchName
 }
 
-function ConvertTo-Base64([string] $Content) {
+function ConvertTo-Base64([string] $Content)
+{
     $bytes = [System.Text.Encoding]::UTF8.GetBytes($Content)
     return [System.Convert]::ToBase64String($bytes)
 }
 
-function ConvertFrom-Base64([string] $Content) {
+function ConvertFrom-Base64([string] $Content)
+{
     $bytes = [System.Convert]::FromBase64String($Content)
     return [System.Text.Encoding]::UTF8.GetString($bytes)
 }
 
-function New-Ctags([string] $Language) {
+function New-Ctags([string] $Language)
+{
     [string]::Join(" ",
         "ctags",
         "--recurse=yes",
@@ -246,5 +297,20 @@ function New-Ctags([string] $Language) {
         "--exclude=node_modules",
         "--totals=yes"
     ) | Invoke-Expression
+}
+
+function Invoke-PSScriptAnalyzer([string] $Path)
+{
+    $params = @{
+        Path = $Path
+        Recurse = $True
+        ExcludeRule = @(
+            "PSUseShouldProcessForStateChangingFunctions",
+            "PSAvoidUsingPositionalParameters",
+            "PSUseSingularNouns"
+        )
+    }
+
+    return Invoke-ScriptAnalyzer @params
 }
 
