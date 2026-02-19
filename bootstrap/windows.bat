c2cc73f698e5f32f54ca6bd41b9b3e712b2fb590
Sasha Gurevich <sagurevich@microsoft.com>
2026-02-19 12:24:41 +0200

    chore(windows): comment with winget installation

--- bootstrap/windows.bat
+++ bootstrap/windows.bat
@@ -1,1 +1,4 @@
+REM If winget is not there:
+REM Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+
 winget install --id Microsoft.Powershell --source winget
