96ea5d3dc40d6f4f7060087ecc9f2226c83c871b
Sasha Gurevich <sagurevich@microsoft.com>
2026-02-20 19:01:57 +0200

    chore: formatting

--- packages/neovim/config/lua/plugins/hopcsharp.lua
+++ packages/neovim/config/lua/plugins/hopcsharp.lua
@@ -2,15 +2,15 @@ local M = {}
 
 M.hopcsharp_menu = function()
     local lists = {
-        { name = 'list all types',  action = require('hopcsharp.pickers.fzf').all_definitions, },
-        { name = 'list classes',    action = require('hopcsharp.pickers.fzf').class_definitions, },
-        { name = 'list interfaces', action = require('hopcsharp.pickers.fzf').interface_definitions, },
+        { name = 'list all types', action = require('hopcsharp.pickers.fzf').all_definitions },
+        { name = 'list classes', action = require('hopcsharp.pickers.fzf').class_definitions },
+        { name = 'list interfaces', action = require('hopcsharp.pickers.fzf').interface_definitions },
         { name = 'list attributes', action = require('hopcsharp.pickers.fzf').attribute_definitions },
-        { name = 'list methods',    action = require('hopcsharp.pickers.fzf').method_definitions },
-        { name = 'list enums',      action = require('hopcsharp.pickers.fzf').enum_definitions },
-        { name = 'list structs',    action = require('hopcsharp.pickers.fzf').struct_definitions },
-        { name = 'list records',    action = require('hopcsharp.pickers.fzf').record_definitions },
-        { name = 'list files',      action = require('hopcsharp.pickers.fzf').source_files },
+        { name = 'list methods', action = require('hopcsharp.pickers.fzf').method_definitions },
+        { name = 'list enums', action = require('hopcsharp.pickers.fzf').enum_definitions },
+        { name = 'list structs', action = require('hopcsharp.pickers.fzf').struct_definitions },
+        { name = 'list records', action = require('hopcsharp.pickers.fzf').record_definitions },
+        { name = 'list files', action = require('hopcsharp.pickers.fzf').source_files },
     }
 
     vim.ui.select(lists, {
