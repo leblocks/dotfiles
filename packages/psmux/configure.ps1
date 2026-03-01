fa78217dd3c596966abd181a57167edfccc28e39
Sasha Gurevich <sagurevich@microsoft.com>
2026-03-01 17:30:40 +0200

    feat(psmux): added ppm installation and plugin configuration

--- packages/psmux/configure.ps1
+++ packages/psmux/configure.ps1
@@ -4,3 +4,12 @@ Test-Dependencies @("psmux", "git")
 
 LinkToHome $PSScriptRoot ".psmux.conf"
 
+# get tmux plugins manager
+if (-Not (Test-Path "$HOME/.tmux/plugins/tpm")) {
+    git clone https://github.com/marlocarlo/psmux-plugins "$env:USERPROFILE\.psmux\plugins\psmux-plugins"
+}
+
+. $env:USERPROFILE\.psmux\plugins\psmux-plugins\ppm\ppm.ps1
+
+Install-AllPlugins
+
