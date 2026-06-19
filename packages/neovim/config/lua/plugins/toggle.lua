3128c193fa6c588be71b6ebbea9a92a51db06766
leblocks <a.f.gurevich@gmail.com>
2026-06-19 22:30:03 +0300

    feat(toggle.nvim): new toggle overrides

--- packages/neovim/config/lua/plugins/toggle.lua
+++ packages/neovim/config/lua/plugins/toggle.lua
@@ -1,8 +1,16 @@
 require('toggle').setup({
     mappings = {
         { 'void', 'Task' },
+        { 'class', 'record', 'struct' },
+        { 'AreEqual', 'AreNotEqual' },
+        { 'AreSame', 'AreNotSame' },
+        { 'Contains', 'DoesNotContain' },
+        { 'StartsWith', 'DoesNotStartWith' },
+        { 'EndsWith', 'DoesNotEndWith' },
+        { 'IsInstanceOfType', 'IsNotInstanceOfType' },
         { 'IsNull', 'IsNotNull' },
         { 'IsTrue', 'IsFalse' },
-        { 'class', 'record', 'struct' },
+        { 'IsEmpty', 'IsNotEmpty' },
+        { 'IsPositive', 'IsNegative' },
     },
 })
