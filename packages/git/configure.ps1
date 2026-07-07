b0b368c992d64fc7fcef08ac7d01a410bc429227
Sasha Gurevich <a.f.gurevich@gmail.com>
2026-07-07 21:49:36 +0300

    added gcm credentials parameter

--- packages/git/configure.ps1
+++ packages/git/configure.ps1
@@ -2,6 +2,8 @@
 
 Test-Dependencies @("git")
 
+Set-EnvironmentVariable GCM_CREDENTIAL_STORE "cache"
+
 # linking .gitconfig to the $HOME is not that great
 # it will override custom defined stuff, so I'll do inlcude instead
 "git config --global include.path `"$(Resolve-Path (Join-Path $PSScriptRoot .gitconfig))`"" | Invoke-FailFastExpression
